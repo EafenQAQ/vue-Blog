8fdaf95ac66909cf3adc47a1b4fcb5065fe43fc6
EafenQAQ <eafenqaq@gmail.com>
2026-01-25 15:24:16 +0800

    chore(build):构建时删除console.log

--- vite.config.js
+++ vite.config.js
@@ -6,13 +6,14 @@ import vueDevTools from 'vite-plugin-vue-devtools'
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [
-    vue(),
-    vueDevTools(),
-  ],
+  plugins: [vue(), vueDevTools()],
   resolve: {
     alias: {
-      '@': fileURLToPath(new URL('./src', import.meta.url))
+      '@': fileURLToPath(new URL('./src', import.meta.url)),
     },
+  },
+  esbuild: {
+    // 在生产环境移除console.log
+    pure: ['console.log'],
   },
 })
